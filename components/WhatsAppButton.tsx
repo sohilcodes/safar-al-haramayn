0cf20dee1bf97e328931e8d920d6de877a99a732
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2026-06-06 15:40:25 +0530

    Create WhatsAppButton.tsx

---
+++ components/WhatsAppButton.tsx
@@ -0,0 +1,11 @@
+export default function WhatsappButton() {
+  return (
+    <a
+      href="https://wa.me/919898325132"
+      target="_blank"
+      className="fixed bottom-6 right-6 bg-green-500 text-white px-5 py-4 rounded-full shadow-lg z-50"
+    >
+      WhatsApp
+    </a>
+  );
+}
