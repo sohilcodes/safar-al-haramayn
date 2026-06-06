fb0aa7ee4b16709c107b6bbe1060c64fe7c55ec5
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2026-06-06 17:29:17 +0530

    Update page.tsx

--- app/group-packages/page.tsx
+++ app/group-packages/page.tsx
@@ -1,29 +1,40 @@
-import PackageCard from "@/components/PackageCard";
 import { getPackages } from "@/lib/packages";
+import PackageCard from "@/components/PackageCard";
 
-export default async function GroupPackagesPage() {
-  const packages = await getPackages();
+export default async function GroupPackagesPage({
+  searchParams,
+}: {
+  searchParams: {
+    city?: string;
+    type?: string;
+    search?: string;
+  };
+}) {
+  const packages = await getPackages(searchParams);
 
   return (
     <main className="max-w-7xl mx-auto px-4 py-20">
 
       <h1 className="text-4xl font-bold mb-10">
-        Group Umrah Packages
+        Packages
       </h1>
 
-      <div className="grid md:grid-cols-3 gap-6">
-        {packages.map((pkg: any) => (
-          <PackageCard
-            key={pkg.id}
-            id={pkg.id}
-            title={pkg.title}
-            departure_city={pkg.departure_city}
-            duration={pkg.duration}
-            price={pkg.price}
-          />
-        ))}
-      </div>
-
+      {packages.length === 0 ? (
+        <p>No packages found 😢</p>
+      ) : (
+        <div className="grid md:grid-cols-3 gap-6">
+          {packages.map((pkg: any) => (
+            <PackageCard
+              key={pkg.id}
+              id={pkg.id}
+              title={pkg.title}
+              departure_city={pkg.departure_city}
+              duration={pkg.duration}
+              price={pkg.price}
+            />
+          ))}
+        </div>
+      )}
     </main>
   );
 }
