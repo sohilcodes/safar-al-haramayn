becc3163894e0798e264b1f88bfefdb818095ce6
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2026-06-06 15:49:49 +0530

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 export default function WhatsappButton() {
   return (
     <a
-      href="https://wa.me/919898325132"
+      href="https://wa.me/919987672403"
       target="_blank"
       className="fixed bottom-6 right-6 bg-green-500 text-white px-5 py-4 rounded-full shadow-lg z-50"
     >
